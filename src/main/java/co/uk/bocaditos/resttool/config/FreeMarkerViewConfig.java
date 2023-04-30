1a0617c7528b1416c3eb4cd3ea84891e4666193b
Amadeo Asco <a.asco@bocaditos.co.uk>
2023-04-30 17:56:15 +0100

    Activa tabs secection

---
+++ src/main/java/co/uk/bocaditos/resttool/config/FreeMarkerViewConfig.java
@@ -0,0 +1,37 @@
+package co.uk.bocaditos.resttool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+
+/**
+ * .
+ * 
+ * @author aasco
+ */
+@Configuration
+public class FreeMarkerViewConfig {
+
+	@Bean 
+	public FreeMarkerViewResolver freemarkerViewResolver() { 
+	    final FreeMarkerViewResolver resolver = new FreeMarkerViewResolver(); 
+
+	    resolver.setCache(true); 
+	    resolver.setPrefix(""); 
+	    resolver.setSuffix(".ftl"); 
+
+	    return resolver; 
+	}
+
+	@Bean 
+	public FreeMarkerConfigurer freemarkerConfig() { 
+	    final FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+
+	    freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/views/ftl/");
+
+	    return freeMarkerConfigurer; 
+	}
+
+} // end class FreeMarkerViewResolver
--- src/main/java/co/uk/bocaditos/resttool/controller/RestToolController.java
+++ src/main/java/co/uk/bocaditos/resttool/controller/RestToolController.java
@@ -4,7 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 /**
@@ -18,10 +20,7 @@ public class RestToolController {
 	private static final Logger logger = LogManager.getLogger(RestToolController.class);
 
 
-	public RestToolController() {
-	}
-
-    @GetMapping({"/", "/home"})
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String home(final Model model) {
     	logger.debug("Setting up home page...");
     	logger.debug("Home page was successfully set up");
@@ -29,7 +28,7 @@ public class RestToolController {
         return "main";
     }
 
-    @GetMapping("/option1")
+    @RequestMapping(value = "/option1", method = RequestMethod.GET)
     public String option1(final Model model) {
     	logger.debug("Setting up Option 1 page...");
     	logger.debug("Option 1 page was successfully set up");
@@ -37,7 +36,7 @@ public class RestToolController {
         return "option_1";
     }
 
-    @GetMapping("/option2")
+    @RequestMapping(value = "/option2", method = RequestMethod.GET)
     public String option2(final Model model) {
     	logger.debug("Setting up Option 2 page...");
     	logger.debug("Option 2 page was successfully set up");
@@ -45,7 +44,7 @@ public class RestToolController {
         return "option_2";
     }
 
-    @GetMapping("/option3")
+    @RequestMapping(value = "/option3", method = RequestMethod.GET)
     public String option3(final Model model) {
     	logger.debug("Setting up Option 3 page...");
     	logger.debug("Option 3 page was successfully set up");
@@ -53,7 +52,7 @@ public class RestToolController {
         return "option_3";
     }
 
-    @GetMapping("/option4")
+    @RequestMapping(value = "/option4", method = RequestMethod.GET)
     public String option4(final Model model) {
     	logger.debug("Setting up Option 4 page...");
     	logger.debug("Option 4 page was successfully set up");
@@ -61,7 +60,7 @@ public class RestToolController {
         return "option_4";
     }
 
-    @GetMapping({"/login"})
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(final Model model) {
     	logger.debug("Setting up login page...");
     	logger.debug("Login page was successfully set up");
@@ -69,7 +68,7 @@ public class RestToolController {
     	return "login_page";
     }
 
-    @GetMapping({"/licence"})
+    @RequestMapping(value = "/licence", method = RequestMethod.GET)
     public String lice(final Model model) {
         return "licence_page";
     }
